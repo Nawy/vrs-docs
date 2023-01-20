a5caaa87c5c7adcc2b8bf78abcf72c0732e4a56e
taiberium <akhmetovrustam1990@gmail.com>
2023-01-20 23:00:52 +0400

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   tagline: 'We make game development easy',
   url: 'https://your-docusaurus-test-site.com',
   //baseUrl: '/',
-  baseUrl: /versolid_docs/,
+  baseUrl: '/versolid_docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
@@ -19,7 +19,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
-  trailingSlash: false,
+  trailingSlash: true,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
